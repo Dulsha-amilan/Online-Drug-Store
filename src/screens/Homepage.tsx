d57648ef484930f6c80a74a0cef0c479128910c0
Senavirathna W.H.D.A <dulsharulzzz@gmail.com>
2024-03-08 18:21:00 +0530

    header crate and icon add

--- src/screens/Homepage.tsx
+++ src/screens/Homepage.tsx
@@ -1,19 +1,75 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+
+import { Icon, SocialIcon } from '@rneui/base'
+import { Ionicons } from '@expo/vector-icons';
 
 const Homepage = () => {
   return (
     <View style={{
         flex:1,
-        justifyContent:'center',
-        alignItems:'center'
+       
     }}>
-      <Text style={{
-        fontSize:30,
-        color:'black'
-      }}>Homepage</Text>
+      <Header/>
+     
     </View>
   )
+}
+const Header =()=>{
+   return(
+    <View style={
+      {
+        flexDirection:"row",
+        padding:'5%',
+
+        alignItems:'center'
+
+      }
+    }>
+      <Image style={
+        {
+          width:40,
+          height:40,
+    
+        }
+      }source={require('../../assets/img/Logo.png')}/>
+
+
+      <View style={
+        {
+          flex:1,
+       
+          alignItems:'center'
+        }
+      }>
+           <Text style={{
+            fontSize:25
+           }}>
+            
+            Jaya Pharmacy</Text>
+           <Text style={{
+            fontSize:16
+           }}>Your Pathway to Health & Wellness.</Text>
+
+      </View>
+<View style={
+  {
+    width:40,
+    height:40,
+  }
+}>
+<Ionicons name="notifications" size={35} color="black" />
+
+
+</View>
+      </View>
+   )
+
+
+
+
+
+
 }
 
 export default Homepage
